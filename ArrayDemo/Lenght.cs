ca94b3d7aff16497689a19ef3904c0fb47a3c186
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-02-10 15:02:42 +0530

    [Ritesh]add :Lenght Of Array

---
+++ ArrayDemo/Lenght.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayDemo
+{
+    public class Lenght
+    {
+        public void Array()
+        {
+            int[] num = { 45, 68, 98, 78, 98, 78 };
+            int array = num.Length;
+            Console.WriteLine(array);
+        }
+    }
+}
---
+++ ArrayDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Lenght array = new Lenght();
+            array.Array();
+            Console.ReadLine();
+        }
+    }
+}
